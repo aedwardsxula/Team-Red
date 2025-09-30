6f2aafe14ba12116cbf70cd55eb467baf5d67a5b
Jayce <jstephe3@xula.edu>
2025-09-29 20:28:46 -0500

    Stored the age, bmi, children, and charges values in seperate ArrayLists

--- evenProblems.java
+++ evenProblems.java
@@ -12,6 +12,10 @@ public class evenProblems {
         String csvPath = "/Users/austin/Documents/CPSC_2735/assignment05/Team-Red/data/insurance.csv";
         String line = "";
         ArrayList<String> data = new ArrayList<>();
+        ArrayList<Integer> ageVals = new ArrayList<>();
+        ArrayList<Double> bmiVals = new ArrayList<>();
+        ArrayList<Integer> childrenVals = new ArrayList<>();
+        ArrayList<Double> chargesVals = new ArrayList<>();
 
         try (BufferedReader reader = new BufferedReader(new FileReader(csvPath))){
             line = reader.readLine(); // Skip header line
@@ -22,7 +26,15 @@ public class evenProblems {
         } catch (IOException e){
             e.printStackTrace();
         }
-        System.out.println(data);
+        for(String c : data){
+            String[] values = c.split(",");
+            ageVals.add(Integer.parseInt(values[0]));
+            bmiVals.add(Double.parseDouble(values[2]));
+            childrenVals.add(Integer.parseInt(values[3]));
+            chargesVals.add(Double.parseDouble(values[6]));
+        }
+
+        
     }
 
     
